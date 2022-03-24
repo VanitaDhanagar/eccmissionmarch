68c306a0315f7b08ab810b9571206c8d976dc559
VanitaDhanagar <80330660+VanitaDhanagar@users.noreply.github.com>
2022-03-24 18:00:49 +0530

    Update service.js

--- srv/service.js
+++ srv/service.js
@@ -12,6 +12,9 @@ module.exports = async srv => {
   messaging.on("refappscf/ecc/123/BO/BusinessPartner/Created", async msg => {
     console.log("<< event caught", msg.event);
     console.log("<< event caught", msg.data);
+    console.log("<< event caught", msg);
+    console.log("<< event caught", msg.data.BUT000[0].PARTNER);
+    
     let BUSINESSPARTNER = "";
     BUSINESSPARTNER = parseInt(msg.data.BUT000[0].PARTNER);
     console.log("<<< Received Created Business Partner Id " + BUSINESSPARTNER);
